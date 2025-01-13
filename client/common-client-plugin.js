1423e0519ab74de533d9e323d020a1fec56ada62
cyberboy666 <langolierz@gmail.com>
2025-01-13 15:42:13 +1300

    small change for new ui layout

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -18,7 +18,7 @@ function findNodeByInnerText(nodelist, innerText){
 }
 
 function hideDefaultSignin (){
-  var loginButton = findNodeByInnerText(document.querySelectorAll('a.orange-button'), 'Login')
+  var loginButton = findNodeByInnerText(document.querySelectorAll('a.peertube-button-link'), 'Login')
   if(loginButton == null) return // return if menu is hidden
   loginButton.style.visibility = "hidden" // hide login button
   var createButton = document.querySelector('a.create-account-button')
@@ -31,7 +31,7 @@ function hideDefaultSignin (){
   if( newJoinButton == null ){
     // create new external-login button
     newJoinButton = document.createElement('a')
-    newJoinButton.setAttribute('href', '/plugins/auth-discourse/0.0.23/auth/discourse-auth') // needs to have the correct version here
+    newJoinButton.setAttribute('href', '/plugins/auth-discourse/0.0.24/auth/discourse-auth') // needs to have the correct version here
     newJoinButton.className = 'new-join-button'
     newJoinButton.innerHTML = 'JOIN'
     newJoinButton.style.cssText = ' display: inline-block; border: none; font-weight: 600; font-size: 15px; height: 30px; line-height: 30px; border-radius: 3px; text-align: center; padding: 0 17px 0 13px; cursor: pointer; display: block; width: 100%; color: #fff; background-color: #fd7e14;'
